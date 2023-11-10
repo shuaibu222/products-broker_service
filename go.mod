1b2179dcc1a15ceaecb0ba76ca6e3e2d5f837457
shuaibu <shuaibuabdulkadir222@gmail.com>
2023-11-10 10:40:09 +0100

    change port

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt/v5 v5.0.0
-	github.com/joho/godotenv v1.5.1
 	github.com/rabbitmq/amqp091-go v1.9.0
 	golang.org/x/crypto v0.13.0
 	google.golang.org/grpc v1.58.3
@@ -19,4 +18,6 @@ require (
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- main.go
+++ main.go
@@ -3,17 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"net/http"
-	"net/smtp"
-	"os"
-	"time"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const (
-	webPort = "80"
+	webPort = "9000"
 )
 
 type Config struct{}
@@ -21,29 +15,7 @@ type Config struct{}
 func main() {
 	log.Printf("Starting broker service on port %s\n", webPort)
 
-	// initialize the connection of rabbitmq
-	conn, err := connect()
-	if err != nil {
-		log.Println("Failed to connect to RabbitMQ", err)
-		os.Exit(1)
-	}
-
-	defer conn.Close()
-
 	app := Config{}
-
-	// Calling consumer function in its goroutine to run concurrently with the http server
-	// and passing rabbitmq connection to it
-	go func() {
-		app.RecivedReviewToRabbitmq(conn)
-	}()
-
-	go func() {
-		err = sendEmail("shuaibuabdulkadir222@gmail.com", "Shuaibu comments on your product", "shuayb")
-		if err != nil {
-			log.Println("Failed to send email", err)
-		}
-	}()
 
 	// define http server
 	server := &http.Server{
@@ -52,61 +24,9 @@ func main() {
 	}
 
 	// start the server
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
 
-}
-
-func sendEmail(email, subject, body string) error {
-	from := os.Getenv("GMAIL_ACCOUNT")
-	password := os.Getenv("GMAIL_SECRET")
-	host := "smtp.gmail.com"
-	port := 587
-
-	// Connect to the SMTP server.
-	auth := smtp.PlainAuth("", from, password, host)
-	to := []string{email}
-	msg := []byte("To: " + email + "\r\n" +
-		"Subject: " + subject + "\r\n" +
-		"\r\n" +
-		"Comment:" + body + "\r\n")
-	err := smtp.SendMail(fmt.Sprintf("%s:%d", host, port), auth, from, to, msg)
-	if err != nil {
-		return nil
-	}
-	log.Println("email is successfully sent to " + email)
-	return err
-}
-
-func connect() (*amqp.Connection, error) {
-	var counts int64
-	var backOff = 1 * time.Second
-	var connection *amqp.Connection
-
-	// don't continue until rabbit is ready
-	for {
-		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
-		if err != nil {
-			fmt.Println("RabbitMQ not yet ready...")
-			counts++
-		} else {
-			log.Println("Connected to RabbitMQ!")
-			connection = c
-			break
-		}
-
-		if counts > 5 {
-			fmt.Println(err)
-			return nil, err
-		}
-
-		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
-		log.Println("backing off...")
-		time.Sleep(backOff)
-		continue
-	}
-
-	return connection, nil
 }
